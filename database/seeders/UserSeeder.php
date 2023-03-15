b6f591591b1afc2357bbc16c80ea1e2e6c85d45d
Alberto-Bruno <brunoalberto97@outlook.it>
2023-03-15 23:49:12 +0100

    first commit

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $user = new User();
+        $user->name = 'Alberto';
+        $user->email = 'Alberto@boolean.it';
+        $user->password = 'password';
+        $user->save();
+    }
+}
